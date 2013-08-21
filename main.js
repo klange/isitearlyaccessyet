b981eca95ffddfe27128df644123a62aed74c499
Kevin Lange <klange@dakko.us>
2013-08-20 21:11:41 -0700

    moogle loading gif

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ var n=14;
 
 window.onload = function() {
     var b=Math.floor(Math.random()*n)+1;
-    document.body.style.backgroundImage = "url(bgs/" + b + ".jpg)";
+    document.getElementById("base").style.backgroundImage = "url(bgs/" + b + ".jpg)";
 }
